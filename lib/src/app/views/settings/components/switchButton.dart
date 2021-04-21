36c8655a343c147b9476ccbd19c9da18e0576fb6
Jontizz96 <lajo1896@student.ju.se>
2021-04-21 17:57:10 +0200

    FEAT: Created switch button widget

---
+++ lib/src/app/views/settings/components/switchButton.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import '../settings_view_model.dart';
+import 'package:lite_rolling_switch/lite_rolling_switch.dart';
+
+
+class SwitchButton extends StatefulWidget {
+
+  final SettingsViewModel _model;
+
+  const SwitchButton(this._model, {Key key}) : super(key: key);
+
+  @override
+  _SwitchButtonState createState() => _SwitchButtonState();
+}
+
+class _SwitchButtonState extends State<SwitchButton> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(19.0),
+                child: Text(
+                  'Simulation mode',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                ),
+              ),
+              Container(
+                height: 40,
+                width: 125,
+                child: LiteRollingSwitch(
+                  value: false,
+                  textOn: 'ON',
+                  textOff: 'OFF',
+                  colorOn: Colors.greenAccent,
+                  colorOff: Colors.redAccent,
+                  iconOn: Icons.done,
+                  iconOff: Icons.power_settings_new,
+                  textSize: 18.0,
+                  onChanged: (bool position) {
+                    print('The button is $position');
+                    // TODO: Change variable state
+                  },
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(19.0),
+                child: Text(
+                  'Manuell steering',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                ),
+              ),
+              Container(
+                height: 40,
+                width: 125,
+                child: LiteRollingSwitch(
+                  value: false,
+                  textOn: 'ON',
+                  textOff: 'OFF',
+                  colorOn: Colors.greenAccent,
+                  colorOff: Colors.redAccent,
+                  iconOn: Icons.done,
+                  iconOff: Icons.power_settings_new,
+                  textSize: 18.0,
+                  onChanged: (bool position) {
+                    print('The button is $position');
+                    // TODO: Change variable state
+                  },
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
