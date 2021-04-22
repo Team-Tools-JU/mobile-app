5ba058f334da255a194b30d7e88898e47bf09358
Jontizz96 <lajo1896@student.ju.se>
2021-04-22 16:39:01 +0200

    FEAT: Added icon library

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_app/src/app/views/settings/settings_view.dart';
 import 'package:mobile_app/src/app/views/start/start_view.dart';
+import 'package:mobile_app/src/app/views/steering/steering_view.dart';
 
 void main() {
   runApp(App());
@@ -28,7 +29,7 @@ class App extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: SettingsView(),
+      home: SteeringView(),
     );
   }
 }
