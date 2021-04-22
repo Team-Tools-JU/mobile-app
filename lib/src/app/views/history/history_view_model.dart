e260c276f634dffb2e4a441e6098e43b6c568a4c
Jontizz96 <lajo1896@student.ju.se>
2021-04-22 17:11:35 +0200

    FEAT: Created history view model

---
+++ lib/src/app/views/history/history_view_model.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+
+class HistoryViewModel extends BaseViewModel {
+
+
+  void init() {}
+
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
