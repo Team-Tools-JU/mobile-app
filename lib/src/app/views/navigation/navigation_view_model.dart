d50d5538a8d7f714d975c29051a9d336f9f25877
Jontizz96 <lajo1896@student.ju.se>
2021-04-23 12:24:37 +0200

    FEAT: Created navigation view

---
+++ lib/src/app/views/navigation/navigation_view_model.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:stacked/stacked.dart';
+
+class NavigationViewModel extends IndexTrackingViewModel {
+
+  String _bluetoothStatusText = 'default';
+  bool _isConnected = false;
+
+  String get bluetoothStatusText => _bluetoothStatusText;
+
+  void init() {
+    _bluetoothStatusText = 'initialised';
+    _isConnected = false;
+    notifyListeners();
+  }
+
+  String updateBluetoothStatus() {
+    if (_isConnected) {
+      _bluetoothStatusText = 'Connected!';
+    } else {
+      _bluetoothStatusText = 'Not connected!';
+    }
+    notifyListeners();
+    return _bluetoothStatusText;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
