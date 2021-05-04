3e6dcbf18bed1f200d02c1e06c004bae2aa1b969
soer1776 <erik.sderstrm@gmail.com>
2021-05-04 11:57:38 +0200

    feat: add BT connection listener in nav VM

--- lib/src/app/views/navigation/navigation_view_model.dart
+++ lib/src/app/views/navigation/navigation_view_model.dart
@@ -1,11 +1,29 @@
+import 'package:flutter_blue/flutter_blue.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mobile_app/src/app/models/implementation/bluetooth_v2.dart';
 import 'package:stacked/stacked.dart';
 import 'package:flutter/material.dart';
 
 class NavigationViewModel extends IndexTrackingViewModel {
-
   String bluetoothStatusText = 'default';
   bool isConnected = false;
   IconData iconData = Icons.bluetooth;
+  Bluetooth _bluetooth = GetIt.I<Bluetooth>();
+
+  NavigationViewModel() {
+    _bluetooth.selectedDevice.state.listen((state) {
+      switch (state) {
+        case BluetoothDeviceState.connected:
+          isConnected = true;
+          break;
+        case BluetoothDeviceState.disconnected:
+          isConnected = false;
+          break;
+        default:
+          break;
+      }
+    });
+  }
 
   String get _bluetoothStatusText => bluetoothStatusText;
   IconData get _bluetoothIcon => iconData;
@@ -20,11 +38,10 @@ class NavigationViewModel extends IndexTrackingViewModel {
   }
 
   IconData updateBluetoothSymbolText() {
-
     if (isConnected) {
       iconData = Icons.bluetooth_connected;
     } else {
-      iconData =Icons.bluetooth_searching;
+      iconData = Icons.bluetooth_searching;
     }
     return _bluetoothIcon;
   }
