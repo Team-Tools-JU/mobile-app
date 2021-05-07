ed69c17bbab75708d537fa356f22f8aa1de28915
Erik <soer1776@student.ju.se>
2021-05-07 11:32:49 +0200

    feat: "impl stateless steer button"

--- lib/src/app/views/steering/steering_view_model.dart
+++ lib/src/app/views/steering/steering_view_model.dart
@@ -2,16 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mobile_app/src/app/models/implementation/bluetooth.dart';
+import 'package:mobile_app/src/app/models/implementation/navigation_controller.dart';
 import 'package:mobile_app/src/app/models/implementation/settings_controller.dart';
 import 'package:mobile_app/src/app/views/navigation/navigation_view.dart';
 import 'package:stacked/stacked.dart';
 
 class SteeringViewModel extends BaseViewModel {
-  SettingsController _controller = GetIt.I<SettingsController>();
+  SettingsController _settingsController = GetIt.I<SettingsController>();
+  NavigationController _navigationController = GetIt.I<NavigationController>();
+
   Bluetooth _bluetooth = GetIt.I<Bluetooth>();
   String bluetoothStatusText = 'default';
   bool isConnected = false;
   IconData iconData = Icons.bluetooth;
+
+  double _signalStrength = 1.0;
+
+  get signalStrength => _signalStrength;
 
   String get _bluetoothStatusText => bluetoothStatusText;
   IconData get _bluetoothIcon => iconData;
@@ -33,6 +40,9 @@ class SteeringViewModel extends BaseViewModel {
         // print(value);
         // readChar = c;
 
+      } else {
+        _navigationController.currentIndex = 0;
+        Get.back();
       }
     });
   }
@@ -61,7 +71,7 @@ class SteeringViewModel extends BaseViewModel {
   }
 
   void disableSteering() {
-    _controller.steeringEnabled.add(false);
+    _settingsController.steeringEnabled.add(false);
     notifyListeners();
     Get.off(NavigationView());
   }
