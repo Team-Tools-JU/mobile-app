027b36c889afeab8d801a0f7f21492c498d90d0b
Jontizz96 <lajo1896@student.ju.se>
2021-05-07 15:02:39 +0200

    UPDATE: Changed name and to stateless

--- lib/src/app/views/history/components/mower_path_stateless.dart
+++ lib/src/app/views/history/components/mower_path_stateless.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_app/src/app/views/history/components/mowerSessions.dart';
+import 'package:mobile_app/src/app/views/history/components/mower_sessions_stateless.dart';
 import 'package:mobile_app/src/app/views/history/components/pathPainter.dart';
 import 'package:mobile_app/src/app/views/history/history_view_model.dart';
 
-class MapWidget extends StatefulWidget {
+class MowerPathStateless extends StatelessWidget {
+  
   final HistoryViewModel model;
-  const MapWidget(this.model, {Key? key}) : super(key: key);
+  const MowerPathStateless(this.model, {Key? key}) : super(key: key);
 
-  @override
-  _MapWidgetState createState() => _MapWidgetState();
-}
-
-class _MapWidgetState extends State<MapWidget> {
   @override
   Widget build(BuildContext context) {
 
     return Column(
       children: [
-        MowerSessions(),
+        MowerSessionsStateless(model),
         Padding(
           padding: EdgeInsets.all(200.0),
           child: Container(
@@ -30,3 +26,6 @@ class _MapWidgetState extends State<MapWidget> {
     );
   }
 }
+
+
+
