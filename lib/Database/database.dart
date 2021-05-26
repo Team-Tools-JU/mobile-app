223a6953fb802f0d1741c6a764c634789808c828
vida23 <vida23>
2021-05-26 10:45:22 +0200

    DB realtime

--- lib/Database/database.dart
+++ lib/Database/database.dart
@@ -1,109 +1,136 @@
 import 'positionEvent.dart';
 
 // Import the firebase_core and cloud_firestore plugin
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class Database {
-  //CRUD operations for Collision Event
-  CollectionReference collisionHistory =
-      FirebaseFirestore.instance.collection("collisionHistory");
+  final dbRef = FirebaseDatabase.instance.reference();
 
-  Future<void> getAllCollisionEvents() async {
-    QuerySnapshot querySnapshot = await collisionHistory.get();
-    final allEvents = querySnapshot.docs.map((doc) => doc.data()).toList();
-    print(allEvents);
+  void addPositionEvent(PositionEvent positionEvent) {
+    dbRef.child(positionEvent.sessionID.toString()).set({
+      'angle': positionEvent.angle.toString(),
+      'collision': positionEvent.collision.toString(),
+      'lenght': positionEvent.length.toString()
+    });
   }
 
-  Future<void> addCollisionEvent(PositionEvent collisionPosition) {
+  List<String> getAllPositions(String sessionID) {
+    List<String> positions;
+    dbRef.orderByChild(sessionID).once().then((DataSnapshot snapshot) {
+      positions.add(snapshot.value.toString());
+    });
+    return positions;
+  }
+
+  void deletePositionEvent(String sessionID) {
+    dbRef.child(sessionID).remove();
+  }
+
+  void addCollisionEvent(PositionEvent collisionPosition) {
     if (collisionPosition.collision == true) {
-      return collisionHistory
-          .add({
-            //Convert to String before saving
-            'coord_x': collisionPosition.pos_CoordX.toString(),
-            'coord_y': collisionPosition.pos_CoordY.toString(),
-            'date': collisionPosition.positionDateTime
-          })
-          .then((data) => print("Collision event added"))
-          .catchError(
-              (error) => print("Failed to add collision event: $error"));
+      dbRef.push().set({
+        'collisionPositionX': collisionPosition.pos_CoordX.toString(),
+        'collisionPositionY': collisionPosition.pos_CoordY.toString(),
+        'collisionSession': collisionPosition.positionDateTime.toString()
+      });
     }
   }
 
-  Future<void> deleteCollisionEvent(PositionEvent collisionPosition) {
-    return collisionHistory
-        .where('sessionID', isEqualTo: collisionPosition.sessionID)
-        .get()
-        .then((snapshot) {
-      snapshot.docs.first.reference.delete().catchError(
-          (error) => print("Failed to add collision event: $error"));
+  List<void> getAllCollisionEvents() {
+    List<void> collisions;
+    dbRef.orderByKey().once().then((DataSnapshot snapshot) {
+      collisions.add(snapshot.value.toString());
     });
+    return collisions;
   }
 
-  Future<void> updateCollisionEvent(PositionEvent collisionPosition) {
-    return collisionHistory
-        .doc(collisionPosition.sessionID.toString())
-        .update({
-          'coord_x': collisionPosition.pos_CoordX.toString(),
-          'coord_y': collisionPosition.pos_CoordY.toString(),
-          'date': collisionPosition.positionDateTime
-        })
-        .then((value) => print("Collision event updated."))
-        .catchError((error) => print("Failed to update event: $error"));
+  void deleteCollisionEvent(String sessionIDToFind) {
+    dbRef.child(sessionIDToFind).remove();
   }
-
-  Future<PositionEvent> getCollisionEventByID(String sessionID) async {
-    DocumentReference documentReference = collisionHistory.doc(sessionID);
-    PositionEvent retrievedEvent;
-    await documentReference.get().then((snapshot) {
-      retrievedEvent.setXCoord(snapshot.data()['coord_x'].toInt());
-      retrievedEvent.setXCoord(snapshot.data()['coord_y'].toInt());
-      retrievedEvent.setDate(snapshot.data()['date']);
-    });
-    return retrievedEvent;
-  }
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// CRUD operations for Position Event
-  CollectionReference positionHistory =
-      FirebaseFirestore.instance.collection("positionHistory");
-
-  Future<void> getAllPositionEvents() async {
-    QuerySnapshot querySnapshot = await positionHistory.get();
-    final allPositions = querySnapshot.docs.map((doc) => doc.data()).toList();
-    print(allPositions);
-  }
-
-  Future<List> getPositionEventInOrder() async {
-    QuerySnapshot querySnapshot =
-        await positionHistory.orderBy('sessionID', descending: true).get();
-    final positionsInOrder =
-        querySnapshot.docs.map((doc) => doc.data()).toList(growable: true);
-    return positionsInOrder;
-  }
-
-  Future<void> addPositionEvent(PositionEvent positionEvent) {
-    return positionHistory
-        .add({
-          'coord_x': positionEvent.pos_CoordX.toString(),
-          'coord_y': positionEvent.pos_CoordY.toString(),
-          'dateTime': positionEvent.positionDateTime
-        })
-        .then((value) => print("Position event added"))
-        .catchError((error) => print("Failed to add position event: $error"));
-  }
-
-  Future<void> deletePositionEvent(PositionEvent positionEvent) {
-    return positionHistory
-        .where('sessionID', isEqualTo: positionEvent.sessionID)
-        .get()
-        .then((snapshot) {
-      snapshot.docs.first.reference
-          .delete()
-          .catchError((error) => print("Failed to add position event: $error"));
-    });
-  }
-
-  //Should not be able to update a position if mower already been there?
-
 }
+
+/////////////////////////////////////////////////////////////////////////
+//   Future<void> getAllCollisionEvents() async {
+//     QuerySnapshot querySnapshot = await collisionHistory.get();
+//     final allEvents = querySnapshot.docs.map((doc) => doc.data()).toList();
+//     print(allEvents);
+//   }
+
+//   Future<void> deleteCollisionEvent(PositionEvent collisionPosition) {
+//     return collisionHistory
+//         .where('sessionID', isEqualTo: collisionPosition.sessionID)
+//         .get()
+//         .then((snapshot) {
+//       snapshot.docs.first.reference.delete().catchError(
+//           (error) => print("Failed to add collision event: $error"));
+//     });
+//   }
+
+//   Future<void> updateCollisionEvent(PositionEvent collisionPosition) {
+//     return collisionHistory
+//         .doc(collisionPosition.sessionID.toString())
+//         .update({
+//           'coord_x': collisionPosition.pos_CoordX.toString(),
+//           'coord_y': collisionPosition.pos_CoordY.toString(),
+//           'date': collisionPosition.positionDateTime
+//         })
+//         .then((value) => print("Collision event updated."))
+//         .catchError((error) => print("Failed to update event: $error"));
+//   }
+
+//   Future<PositionEvent> getCollisionEventByID(String sessionID) async {
+//     DocumentReference documentReference = collisionHistory.doc(sessionID);
+//     PositionEvent retrievedEvent;
+//     await documentReference.get().then((snapshot) {
+//       retrievedEvent.setXCoord(snapshot.data()['coord_x'].toInt());
+//       retrievedEvent.setXCoord(snapshot.data()['coord_y'].toInt());
+//       retrievedEvent.setDate(snapshot.data()['date']);
+//     });
+//     return retrievedEvent;
+//   }
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// // CRUD operations for Position Event
+//   CollectionReference positionHistory =
+//       FirebaseFirestore.instance.collection("positionHistory");
+
+//   Future<void> getAllPositionEvents() async {
+//     QuerySnapshot querySnapshot = await positionHistory.get();
+//     final allPositions = querySnapshot.docs.map((doc) => doc.data()).toList();
+//     print(allPositions);
+//   }
+
+//   Future<List> getPositionEventInOrder() async {
+//     QuerySnapshot querySnapshot =
+//         await positionHistory.orderBy('sessionID', descending: true).get();
+//     final positionsInOrder =
+//         querySnapshot.docs.map((doc) => doc.data()).toList(growable: true);
+//     return positionsInOrder;
+//   }
+
+//   Future<void> addPositionEvent(PositionEvent positionEvent) {
+//     return positionHistory
+//         .add({
+//           'coord_x': positionEvent.pos_CoordX.toString(),
+//           'coord_y': positionEvent.pos_CoordY.toString(),
+//           'dateTime': positionEvent.positionDateTime
+//         })
+//         .then((value) => print("Position event added"))
+//         .catchError((error) => print("Failed to add position event: $error"));
+//   }
+
+//   Future<void> deletePositionEvent(PositionEvent positionEvent) {
+//     return positionHistory
+//         .where('sessionID', isEqualTo: positionEvent.sessionID)
+//         .get()
+//         .then((snapshot) {
+//       snapshot.docs.first.reference
+//           .delete()
+//           .catchError((error) => print("Failed to add position event: $error"));
+//     });
+//   }
+
+//   //Should not be able to update a position if mower already been there?
+
+// }
