eca1e9eef5adcb2efb72196080eeba6fe6e38c3b
Erik <soer1776@student.ju.se>
2021-05-27 18:10:08 +0200

    feat: add scale var & remove unused code

--- lib/src/app/views/history/components/pathPainter.dart
+++ lib/src/app/views/history/components/pathPainter.dart
@@ -21,25 +21,21 @@ class PathPainter extends CustomPainter {
 
     Path path = Path();
 
+    double scale = 0.3;
+
     if (positionEvents.isNotEmpty) {
       PositionEvent origin = positionEvents[0];
-      path.moveTo(origin.pos_CoordX, origin.pos_CoordY);
+      path.moveTo(origin.pos_CoordY * scale, origin.pos_CoordX * scale);
       for (PositionEvent position in positionEvents) {
-        path.lineTo(position.pos_CoordX, position.pos_CoordY);
+        path.lineTo(position.pos_CoordY * scale, position.pos_CoordX * scale);
 
         if (position.collision == 1) {
-          canvas.drawCircle(Offset(position.pos_CoordX, position.pos_CoordY), 6,
+          canvas.drawCircle(
+              Offset(position.pos_CoordY * scale, position.pos_CoordX * scale),
+              6,
               collisionPaint);
         }
       }
-
-      //canvas.scale(x_scale, y_scale);
-      //This allows us to get the bounds of the path we will paint, don't know how to scale it to fit the canvas..
-      Rect bounds = path.getBounds();
-      var left = bounds.left;
-      var right = bounds.right;
-      var top = bounds.top;
-      var bottom = bounds.bottom;
 
       canvas.drawPath(path, pathPaint);
     }
@@ -48,50 +44,3 @@ class PathPainter extends CustomPainter {
   @override
   bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
-/*
-
-//TODO: this should be removed once we get real data
-  List<PositionEvent> getPositions(int sessionID) {
-    final positionEvents = [
-      PositionEvent(100, 200, false),
-      PositionEvent(-50, 200, true),
-      PositionEvent(58, 160, false),
-      PositionEvent(-150, 34, false),
-      PositionEvent(-160, -200, false),
-      PositionEvent(-100, -70, true),
-      PositionEvent(-100, -200, false),
-    ];
-
-    return positionEvents;
-  }
-}
-
-//TODO: remove once we get the real class and get the data dynamically
-class PositionEvent {
-  double positionCoord_x;
-  double positionCoord_y;
-  bool collision;
-
-  PositionEvent(this.positionCoord_x, this.positionCoord_y, this.collision);
-
-  bool getCollision() {
-    return this.collision;
-  }
-
-  double getXCoord() {
-    return this.positionCoord_x;
-  }
-
-  double getYCoord() {
-    return this.positionCoord_y;
-  }
-
-  void setXCoord(double newXCoord) {
-    this.positionCoord_x = newXCoord;
-  }
-
-  void setYCoord(double newYCoord) {
-    this.positionCoord_y = newYCoord;
-  }
-}
-*/
